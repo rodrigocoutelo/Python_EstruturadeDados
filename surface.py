95d8947db095e3ff6e907af32f917402ac309a57
Rodrigo Coutelo <rodrigo@coutelo.me>
2022-03-06 20:55:47 -0300

    ammend

--- surface.py
+++ surface.py
@@ -26,12 +26,12 @@ class Surface(object):
           point = Point(matrix, line, column)
           if point.get_point() == surface_type.code:
             start_point = point
-            size_of_area = self.is_surface(start_point,surface_type)
+            size_of_area = self.is_surface_of_type(start_point,surface_type)
             result.append(size_of_area)
             result.sort()
     return result
 
-  def is_surface(self, point:Point, surface_type:SurfaceType, size_of_area=0):
+  def is_surface_of_type(self, point:Point, surface_type:SurfaceType, size_of_area=0):
     if point.get_point() == surface_type.code:
       point.set_point(surface_type.legend)
       size_of_area += 1
@@ -41,19 +41,19 @@ class Surface(object):
   def check_extension(self, point:Point,surface_type,current_size_of_area):
     #up
     if not point.is_in_first_line:
-      current_size_of_area =  self.is_surface(point.move_up(),surface_type, current_size_of_area)
+      current_size_of_area =  self.is_surface_of_type(point.move_up(),surface_type, current_size_of_area)
 
     #down
     if not point.is_in_last_line:
-      current_size_of_area = self.is_surface(point.move_down(),surface_type, current_size_of_area)
+      current_size_of_area = self.is_surface_of_type(point.move_down(),surface_type, current_size_of_area)
 
     #left
     if not point.is_in_first_column:
-      current_size_of_area = self.is_surface(point.move_left(),surface_type, current_size_of_area)
+      current_size_of_area = self.is_surface_of_type(point.move_left(),surface_type, current_size_of_area)
 
     #right
     if not point.is_in_last_column:
-      current_size_of_area = self.is_surface(point.move_right(),surface_type, current_size_of_area)
+      current_size_of_area = self.is_surface_of_type(point.move_right(),surface_type, current_size_of_area)
 
     return current_size_of_area
 
